ff403df9cbe57892796cd9298cefbaa9ee1698df
Darshan <home@Darshans-MacBook-Air.local>
2026-05-11 23:31:01 +0530

    Deploy: Mon May 11 23:31:01 IST 2026

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Configuration
-PROJECT_DIR="\$HOME/tiffinset"
+PROJECT_DIR="tiffinset"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 VM_NAME="tiffinset-app"
 ZONE="asia-south1-a"
@@ -17,8 +17,8 @@ git push origin $BRANCH
 
 # 2 & 3. SSH into VM and deploy
 echo "🔌 Connecting to GCP VM and deploying..."
-gcloud compute ssh $VM_NAME --zone=$ZONE --ssh-flag="-A" --command "
-  cd $PROJECT_DIR && \
+gcloud compute ssh $VM_NAME --zone=$ZONE --command "
+  cd ~/$PROJECT_DIR && \
   git fetch origin $BRANCH && \
   git reset --hard origin/$BRANCH && \
   npm ci --production && \
